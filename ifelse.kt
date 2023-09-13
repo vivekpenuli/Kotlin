c8cb3e8a8b0237f4229c47e3b3574872b7e165c2
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-09-13 13:11:11 +0530

    Update ifelse.kt

--- ifelse.kt
+++ ifelse.kt
@@ -9,3 +9,23 @@ fun main()
 }
 
 
+---------------------------------------
+// if Else Return 
+    fun main()
+{
+    val age= 30
+val text =    if(age>40){
+        println("hy guys")
+    "ok every one"}
+    else{
+        println("go ")
+    " so what is cooking "
+    }
+
+    println(" the output is ${text}")
+
+
+}
+
+
+
