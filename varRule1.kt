4e7cbaae2f26d1dc28311bb8018327298f917cf6
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-09-13 12:49:05 +0530

    Update varRule1.kt

--- varRule1.kt
+++ varRule1.kt
@@ -1,6 +1,27 @@
+
 var name
 name ="hy"
 println(${name})
 
 ------------------------------
- // such type of var declaration is not allowed 
+ // such type of var declaration is not allowed [ This variable must either have a type annotation or be initialized ]
+
+
+// Solution :
+//1.  Define the data type of the variable  
+
+ var name :String
+name ="hy"
+println(${name})
+
+--------------------------------------------
+
+ 
+ var name : String 
+println(${name})
+
+// Variable 'name' must be initialized
+// so whenever you try to use the varaible without initalizign it in the program you will get compile type error :
+
+
+---------------------------------------------------
