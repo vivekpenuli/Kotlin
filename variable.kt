4f112fe4eac950a1dd67012537714e61d4dd3c86
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-09-14 22:15:07 +0530

    Update variable.kt

--- variable.kt
+++ variable.kt
@@ -1,5 +1,5 @@
 fun main()
 {
-  var fullname : String ="my name is"  // define Data Type also. 
+  var fullname : String ="my name is"  // define Data Type also.  like this 
   var name ="vivek"    // Automatically Data Type Detection
 }
