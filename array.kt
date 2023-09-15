5554927fc5b9e37357f4cf59d3594ed0f1eb9f69
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-09-15 18:01:57 +0530

    Update array.kt

--- array.kt
+++ array.kt
@@ -1,1 +1,42 @@
 
+fun main()
+{
+    val myArr= arrayOf(1,2,3,4)     // These 3 line denote different way of array declaration 
+    val a1= IntArray(5)
+    val a2= Array<Int>(5){0}
+//---------------------------------------------------------
+  
+   a1.forEach { println(it) }           
+  
+   for (i in myArr)
+   {
+       println("the elements are ${i}")
+   }
+
+
+
+    ------------------------ ///
+
+ val o =my(arrayOf(1,2,3,4,5))
+}
+
+//fun go(number : Array<Int>):Int
+//{
+//    var sum =0
+//    for( i in number)
+//    {
+//        sum +=i
+//    }
+//    return sum
+//}
+
+fun my(number :Array<Int>):Array<Int>
+{
+    var sum =0
+    for(i in number)
+    {
+        sum +=i
+    }
+
+    return number
+}
