40ffaf2ab329470c59a1dac72a8b6b2509e60d91
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-09-15 18:46:34 +0530

    Update paramteroops.kt

--- paramteroops.kt
+++ paramteroops.kt
@@ -1,1 +1,15 @@
+class abc(var name:String, var rool:Int){
+    fun display()
+    {
+        println("The student name is ${name}")
+        println("the roll number of student is ${rool}")
+    }
+}
 
+fun main()
+{
+    val a = abc("vivek",23)
+    val b = abc("sumit",67)
+    a.display()
+    b.display()
+}
