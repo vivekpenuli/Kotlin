d3489e27db7279442a79064ec998ab4cde999876
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-09-15 18:07:05 +0530

    Update function.kt

--- function.kt
+++ function.kt
@@ -1,1 +1,19 @@
+fun main()
+{
+  hy()
+   println(go(5,7))
+    println(so(5,7))
 
+}
+fun hy()
+{
+    println("this me ")
+}
+
+fun go(a:Int,b:Int): Int
+{ //  a=67   cant' reasign value
+    return a+b
+}
+
+//------------------
+fun so(a:Int, b:Int,c:Int =67) = a+b
