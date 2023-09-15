0ee46dbded0a93ab4f653953cdd197d3e9e0c15b
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-09-15 18:53:23 +0530

    Update init.kt

--- init.kt
+++ init.kt
@@ -1,1 +1,22 @@
+class abc2(var name :String, var age:Int){
+    init {
+        if(name.equals("vivek"))
+            println("you eneter name is correct")
+        else
+            println("enter correct name")
+    }
 
+    fun outputname()
+    {
+        println("the name is ${name}")
+        println("the roll no is ${age}}")
+    }
+}
+
+fun main()
+{
+    val c= abc2("vivek",23)
+    c.outputname()
+    val d= abc2("kapil",56)
+    d.outputname()
+}
