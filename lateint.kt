715564426bc228516ad0b53542ebaae2b322ce82
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-09-16 19:58:47 +0530

    Update lateint.kt

--- lateint.kt
+++ lateint.kt
@@ -1,1 +1,11 @@
+fun main()
+{
 
+    lateinit var myProperty: String
+    myProperty="hello"
+    var a : String
+a="hello woeld"
+ //   println("hello b is ${b}")
+    println("hello a is ${a}")
+
+}
