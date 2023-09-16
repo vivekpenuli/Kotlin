2c5161371ba788b76a0b9507e23686d110d9dda6
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-09-16 20:03:53 +0530

    Update compainon.kt

--- compainon.kt
+++ compainon.kt
@@ -1,1 +1,19 @@
+class vivek{
+    companion object{
+        var a=0
+        fun hello()
+        {
+            println("hello i am companio called me ${a}")
+        }
+    }
+    fun hello()
+    {
+        println("so i am ")
+    }
+}
 
+fun main() {
+    val a=vivek()
+    a.hello()
+    vivek.hello()
+}
