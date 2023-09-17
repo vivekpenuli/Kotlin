52ca95473325d0ef1f407acebecda8c2ca0dd52b
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-09-17 17:17:03 +0530

    Update Inheritance.kt

--- Inheritance.kt
+++ Inheritance.kt
@@ -1,1 +1,19 @@
+open class a{
+    fun run()
+    {
+        println("this is class a ")
+    }
+}
+class B: a(){
+    fun op()
+    {
+        println("i am class b")
+    }
+}
 
+fun main()
+{
+    val o =B()
+    o.run()
+
+}
