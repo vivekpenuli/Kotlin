463529a86c8ac4cc74d12ee0f63962aa3f1c250f
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-09-19 19:49:00 +0530

    Update abstract.kt

--- abstract.kt
+++ abstract.kt
@@ -1,1 +1,20 @@
+abstract class a1{
+    fun run()
+    {
 
+    }
+   abstract fun hello()
+}
+class b2: a1(){
+    override fun hello() {
+        TODO("Not yet implemented")
+    }
+}
+
+fun main()
+{
+//    val obje1= a1()   // Not object possible of abstract class
+    val obj2=b2()
+    obj2.hello()
+ obj2.run()
+}
