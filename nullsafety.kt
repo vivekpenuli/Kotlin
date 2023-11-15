bcf25c85914aeab94852bd854ac26172ebad4e9f
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-11-15 12:57:53 +0530

    Update nullsafety.kt

--- nullsafety.kt
+++ nullsafety.kt
@@ -9,6 +9,16 @@ var name :String ? = null
     print("the name is ${name.length}")
 }
 
-//Only safe (?.) or non-null asserted (!!.) calls are allowed on a nullable receiver of type String?
+// Output :Only safe (?.) or non-null asserted (!!.) calls are allowed on a nullable receiver of type String?
 
 
+case 2:
+/**
+ * You can edit, run, and share this code.
+ * play.kotlinlang.org
+ */
+fun main() {
+var name :String ? = null
+    print("the name is ${name?.length}")
+}
+//Output : the name is null
