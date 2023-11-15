d95eed0d5d602c2482b629ac55f7cb1a74197126
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-11-15 12:59:06 +0530

    Update nullsafety.kt

--- nullsafety.kt
+++ nullsafety.kt
@@ -22,3 +22,21 @@ var name :String ? = null
     print("the name is ${name?.length}")
 }
 //Output : the name is null
+
+
+case 3:
+/**
+ * You can edit, run, and share this code.
+ * play.kotlinlang.org
+ */
+fun main() {
+var name :String ? = null
+    print("the name is ${name!!.length}")
+}
+/*
+Exception in thread "main" java.lang.NullPointerException
+ at FileKt.main (File.kt:7) 
+ at FileKt.main (File.kt:-1) 
+ at jdk.internal.reflect.NativeMethodAccessorImpl.invoke0 (:-2) 
+*/
+
