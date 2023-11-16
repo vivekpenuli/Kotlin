c75aebc5dab82bc5039d6db6fbb338939459bbb3
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-11-16 14:53:30 +0530

    Update classinKotlin.kt

--- classinKotlin.kt
+++ classinKotlin.kt
@@ -1,1 +1,22 @@
+class Abc(var name :String ,
+         var age :Int,
+         var Id:Int)
+{
+  fun show()
+  {
+  println("the name of the student is ${name}")
+  println("the age of the student  is ${age}")
+  println("the ID of the student is ${name}")  
+  }
+}
+fun main()
+{
+  var a1=Abc("vivek",45,101)
+  // var a2=Abc("sumit",35,102)
+  // var a3=Abc("gopal",85,103)
+  // var a4=Abc("anirudh",25,104)
 
+  a1.show()
+  
+  
+}
