249531eaa33a70efcc0d85741efeffd513c688e6
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-11-16 20:08:02 +0530

    Update inheritance1.kt

--- inheritance1.kt
+++ inheritance1.kt
@@ -1,1 +1,19 @@
-o
+open class a{
+    fun show()
+    {
+        print("hello friends")
+    }
+}
+class b : a()
+{
+    fun show2()
+    {
+        print("hello guys")
+    }
+}
+fun main()
+{
+    var abc= b();
+    abc.show();
+    abc.show2()
+}
