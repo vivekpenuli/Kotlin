d6850fb7c034d6c91a058313f96545fd22bbd5e2
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-11-16 20:33:38 +0530

    Update ListinSet.kt

--- ListinSet.kt
+++ ListinSet.kt
@@ -1,1 +1,12 @@
+ fun main() {
+     val ls1=mutableListOf(1,2,3,4)
+     val ls2=mutableListOf(4,6,7,8)
+     val ls3=mutableListOf(11,21,31,9)
+     val ls4=mutableListOf(112,23,3,4)
+     val ls5=mutableListOf(1,2,3,4)
 
+    val ls = setOf(ls1,ls2,ls3,ls4,ls5)
+    ls.forEach{
+        println(it)
+    }
+}
