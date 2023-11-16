8ad2d50705335f69070c484da611ea15d412fe0d
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-11-16 17:43:25 +0530

    Update lateint.kt

--- lateint.kt
+++ lateint.kt
@@ -18,3 +18,32 @@ a="hello woeld"
 // a= 23
 
 // here also doing lateinit
+
+// No -- lateintit vs var a: String is not same
+/*
+lateinint concept used in class where var a: String concept will not work this initalization concept is till function , but 
+when it come to class varaible it should be sone via lateinit if you dont do this way you will get compilation error and your programw will not run */
+
+class Abc(var name :String ,
+         var age :Int,
+         var Id:Int)
+{
+    lateinit var clas:String
+  fun show()
+  {
+  println("the name of the student is ${name}")
+  println("the age of the student  is ${age}")
+  println("the ID of the student is ${name}")  
+  }
+}
+fun main()
+{
+  var a1=Abc("vivek",45,101)
+  // var a2=Abc("sumit",35,102)
+  // var a3=Abc("gopal",85,103)
+  // var a4=Abc("anirudh",25,104)
+
+  a1.show()
+  
+  
+}
