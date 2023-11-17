ddf389d6d43bce4c9e623ec6b38affadb5058b1a
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-11-17 13:58:21 +0530

    Update emptlylit.kt

--- emptlylit.kt
+++ emptlylit.kt
@@ -1,1 +1,13 @@
-
+fun main() {
+    val name = mutableListOf<Int>()
+    name.add(1)
+    name.add(2)
+    name.add(3)
+    name.add(4)
+    name.add(5)
+name.forEach{
+    value -> println("the value are ${value}")
+}
+    
+    
+}
