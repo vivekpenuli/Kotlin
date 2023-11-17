740bebb9047c8b6813c913b5a27d9add62cc3bca
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-11-17 14:06:10 +0530

    Update Map2.kt

--- Map2.kt
+++ Map2.kt
@@ -1,1 +1,15 @@
+fun main() {
+val mp = mutableMapOf<Int,List<Int>>()
+mp.put(1,listOf(1,2,3,4))
+mp.put(2,listOf(1,2,3,4))
+mp.put(3,listOf(1,2,6,4))
+mp.put(4,listOf(1,7,3,4))
+mp.put(5,listOf(9,2,3,4))
+mp.put(6,listOf(2,3,6,4))
 
+mp.forEach{
+    key , value ->
+    println("the element key is ${key} and value is ${value}")
+
+}
+}
