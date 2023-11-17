d285f57cdeded77db247af2ea3e81fb2a4be0829
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-11-17 13:44:33 +0530

    Update mapTransformer.kt

--- mapTransformer.kt
+++ mapTransformer.kt
@@ -1,1 +1,19 @@
-
+fun main() {
+    val ls= mutableListOf(1,2,3,4,6,8)
+    ls.map{
+        value ->
+        if(value % 2==0)
+        {
+           value*12
+        }
+        else{
+            value
+        }
+    }
+    
+    ls.forEach{
+        value ->
+        println("the value are ${value}")
+    }
+    
+}
